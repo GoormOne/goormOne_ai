b97888a68e87861f279a00eb0a5bde6e155382e8
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2025-08-25 15:13:14 +0900

    feat: Add the log params for ai #5

--- msa-ai-service/app/main.py
+++ msa-ai-service/app/main.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI(title="MSA AI Service")
 
+
 app.include_router(health.router, prefix="/ai", tags=["health"])
 app.include_router(seed_router.router, prefix="/ai", tags=["seed"])
 app.include_router(qa_router.router, prefix="/ai", tags=["qa"])
