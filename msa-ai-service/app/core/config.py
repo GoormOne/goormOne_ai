98f17b9fda8a106de1becd7e60e671dcd640cf8d
miiiniii <sunmin1997@gmail.com>
2025-09-12 16:34:24 +0900

    chore: change config.py #21

--- msa-ai-service/app/core/config.py
+++ msa-ai-service/app/core/config.py
@@ -12,6 +12,12 @@ else:
 
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 MONGODB_NAME = os.getenv("MONGODB_NAME", "ai_service_db")
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_REQUEST_STREAM = os.getenv("REDIS_REQUEST_STREAM", "chat:requests")
+REDIS_RESPONSE_STREAM = os.getenv("REDIS_RESPONSE_STREAM", "chat:responses")
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") 
 
 REVIEW_LABELS = ["quantity", "size", "sweet", "salty", "spicy", "deep", "sour"]
